74306c8b08c29e096d607dd29e6db642d5e115f3
ariefbara <arief@barapraja.com>
2020-08-26 20:01:03 +0700

    Update PersonalStoreItem.js

--- components/store/PersonalStoreItem.js
+++ components/store/PersonalStoreItem.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
-import { Card, Button, Paragraph } from "react-native-paper";
+import { Avatar, Card, Button, Paragraph } from "react-native-paper";
+import { theme } from "../../App";
 
 import Colors from "../../constants/colors";
+
+const LeftContent = props => <Avatar.Icon {...props} icon="store" theme={theme} />
 
 const PersonalStoreItem = (props) => {
   return (
     <Card style={{ margin: 5 }}>
-      <Card.Title title={props.name} />
+      <Card.Title title={props.name} subtitle="Store Created : 25 June 2020" left={LeftContent} />
       <Card.Actions>
         <Button
-          mode="contained"
+          mode="text"
           color={Colors.primary}
           onPress={props.gotoGoods}
         >
-          Goods
+          My Product
         </Button>
       </Card.Actions>
     </Card>
